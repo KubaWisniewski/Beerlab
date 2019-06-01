af957927d268cbd92cb33a2a337016d7f7f5fa1c
DESKTOP-9MD9SOV\Kuba <kubaw1996@gmail.com>
2019-06-01 15:48:08 +0200

    Adding models to user login/register

---
+++ src/main/java/com/app/model/RoleName.java
@@ -0,0 +1,6 @@
+package com.app.model;
+
+public enum RoleName {
+    ROLE_USER,
+    ROLE_ADMIN
+}
---
+++ src/main/java/com/app/model/User.java
@@ -0,0 +1,32 @@
+package com.app.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.time.LocalDate;
+import java.util.HashSet;
+import java.util.Set;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Entity
+public class User {
+    @Id
+    @GeneratedValue
+    private Long id;
+    private String username;
+    @Column(unique = true)
+    private String email;
+    private String password;
+    private LocalDate dataOfBirth;
+    @ManyToMany(fetch = FetchType.EAGER)
+    @JoinTable(name = "user_roles",
+            joinColumns = @JoinColumn(name = "user_id"),
+            inverseJoinColumns = @JoinColumn(name = "role_id"))
+    private Set<Role> roles = new HashSet<>();
+}
