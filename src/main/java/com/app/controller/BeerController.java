f8dcbb39482988e9db717d9baf2eae7cbbc85866
DESKTOP-9MD9SOV\Kuba <kubaw1996@gmail.com>
2019-06-17 00:07:27 +0200

    Beer crud

---
+++ src/main/java/com/app/controller/BeerController.java
@@ -0,0 +1,42 @@
+package com.app.controller;
+
+import com.app.model.Beer;
+import com.app.service.BeerService;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/beer")
+public class BeerController {
+    private BeerService beerService;
+
+    public BeerController(BeerService beerService) {
+        this.beerService = beerService;
+    }
+
+    @GetMapping
+    public List<Beer> getAllBeers() {
+        return beerService.getBeers();
+    }
+
+    @PostMapping
+    public Beer addBeer(@RequestBody Beer beer) {
+        return beerService.addBeer(beer);
+    }
+
+    @GetMapping("/{id}")
+    public Beer getBeer(@PathVariable Long id) {
+        return beerService.getBeer(id);
+    }
+
+    @DeleteMapping("/{id}")
+    public Beer deleteBeer(@PathVariable Long id) {
+        return beerService.deleteBeer(id);
+    }
+
+    @PutMapping
+    public Beer updateBeer(@RequestBody Beer beer) {
+        return beerService.updateBeer(beer);
+    }
+}
--- src/main/java/com/app/controller/UserController.java
+++ src/main/java/com/app/controller/UserController.java
@@ -3,9 +3,11 @@ package com.app.controller;
 import com.app.repository.UserRepository;
 import com.app.security.CurrentUser;
 import com.app.security.UserPrincipal;
+import io.swagger.annotations.ApiParam;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import springfox.documentation.annotations.ApiIgnore;
 
 @RestController
 @RequestMapping("/api/user")
@@ -17,7 +19,7 @@ public class UserController {
     }
 
     @GetMapping("/me")
-    public String getUserInformation(@CurrentUser UserPrincipal userDetails) {
+    public String getUserInformation(@ApiIgnore @CurrentUser UserPrincipal userDetails) {
         return userDetails.getUsername();
     }
 }
