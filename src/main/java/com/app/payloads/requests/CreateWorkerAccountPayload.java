4c4238c4b973f84034b738583b7660ff2e908a27
DESKTOP-9MD9SOV\Kuba <kubaw1996@gmail.com>
2019-11-04 20:11:44 +0100

    Refactor

--- src/main/java/com/app/payloads/requests/CreateWorkerAccountPayload.java
+++ src/main/java/com/app/payloads/requests/CreateWorkerAccountPayload.java
@@ -1,7 +1,9 @@
 package com.app.payloads.requests;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import javax.validation.constraints.Email;
 import javax.validation.constraints.NotBlank;
@@ -9,6 +11,8 @@ import javax.validation.constraints.NotBlank;
 
 @Data
 @Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class CreateWorkerAccountPayload {
     @NotBlank
     private String username;
--- src/main/java/com/app/payloads/requests/LoginPayload.java
+++ src/main/java/com/app/payloads/requests/LoginPayload.java
@@ -1,12 +1,16 @@
 package com.app.payloads.requests;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import javax.validation.constraints.NotBlank;
 
 @Data
 @Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class LoginPayload {
     @NotBlank
     private String email;
