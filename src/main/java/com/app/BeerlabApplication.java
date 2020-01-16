e2149afcde3198c2fc7bccd693f77d94023d8dae
Daniel Belter <danielbelter96@gmail.com>
2020-01-17 00:28:39 +0100

    WIP

--- src/main/java/com/app/BeerlabApplication.java
+++ src/main/java/com/app/BeerlabApplication.java
@@ -1,16 +1,24 @@
 package com.app;
 
+import com.app.model.Beer;
+import com.app.model.Role;
+import com.app.model.RoleName;
+import com.app.model.User;
 import com.app.repository.BeerRepository;
 import com.app.repository.RoleRepository;
 import com.app.repository.UserRepository;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.data.jpa.convert.threeten.Jsr310JpaConverters;
 import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.session.jdbc.config.annotation.web.http.EnableJdbcHttpSession;
 
 import javax.annotation.PostConstruct;
+import java.util.Arrays;
+import java.util.Collections;
 import java.util.TimeZone;
 
 @SpringBootApplication
@@ -33,29 +41,24 @@ public class BeerlabApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(BeerlabApplication.class, args);
+
     }
 
     @PostConstruct
     void init() {
         TimeZone.setDefault(TimeZone.getTimeZone("CET"));
     }
+/*    @Override
+    public void run(String... args) throws Exception {
+        if (roleRepository.count() != RoleName.values().length) {
+            roleRepository.deleteAll();
+            Arrays
+                    .stream(RoleName.values())
+                    .forEach(role -> roleRepository.save(Role.builder().roleName(role).build()));
+        }
 
-//    public void run(String... args) throws Exception {
-//        if (roleRepository.count() != RoleName.values().length) {
-//            roleRepository.deleteAll();
-//            Arrays
-//                    .stream(RoleName.values())
-//                    .forEach(role -> roleRepository.save(Role.builder().roleName(role).build()));
-//        }
-//        if (beerRepository.count() == 0) {
-//            Beer beer = Beer.builder().brand("Aaaa").description("ADesc").price(10.0).build();
-//            Beer beer2 = Beer.builder().brand("Bbbb").description("BDesc").price(10.0).build();
-//            beerRepository.saveAll(Arrays.asList(beer, beer2));
-//        }
-//
-//        BCryptPasswordEncoder bCryptPasswordEncoder = new BCryptPasswordEncoder();
-//        userRepository.save(User.builder().email("test@test.com").username("test").roles(Collections.singleton(roleRepository.findByRoleName(RoleName.ROLE_USER).get())).password(bCryptPasswordEncoder.encode("123")).build());
-//        userRepository.save(User.builder().email("admin@test.com").username("admin").roles(Collections.singleton(roleRepository.findByRoleName(RoleName.ROLE_ADMIN).get())).password(bCryptPasswordEncoder.encode("123")).build());
-//
-//    }
+        BCryptPasswordEncoder bCryptPasswordEncoder = new BCryptPasswordEncoder();
+        userRepository.save(User.builder().email("manager@beerlab.com").username("manager").roles(Collections.singletonList(roleRepository.findByRoleName(RoleName.ROLE_ADMIN).get())).password(bCryptPasswordEncoder.encode("manager")).build());
+        userRepository.save(User.builder().email("barman@beerlab.com").username("barman").roles(Collections.singletonList(roleRepository.findByRoleName(RoleName.ROLE_BARMAN).get())).password(bCryptPasswordEncoder.encode("barman")).build());
+    }*/
 }
